c19eedc8d8c218c99fa7603d9c0de51ef320b9f1
lcoombe <lauren.e.coombe@gmail.com>
2026-06-11 08:27:22 -0700

    Fix for pylint

--- bin/ntsynt_viz.py
+++ bin/ntsynt_viz.py
@@ -116,7 +116,7 @@ def main():
 
     if args.name_conversion:
         check_name_conversion(args.name_conversion, parser)
-        
+
     if args.target_genome and args.order:
         parser.error("Please specify only one of --target-genome or --order, not both.")
 

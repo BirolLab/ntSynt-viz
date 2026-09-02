41a5c4a70751320992c8f343ca6363617a968549
lcoombe <lauren.e.coombe@gmail.com>
2026-09-02 15:04:02 -0700

    Prepare for release v1.1.1

--- bin/ntsynt_viz.py
+++ bin/ntsynt_viz.py
@@ -7,7 +7,7 @@ import shlex
 import subprocess
 import os
 
-NTSYNT_VIZ_VERSION = "1.1.0"
+NTSYNT_VIZ_VERSION = "1.1.1"
 
 def read_fai_files(fai_file):
     "Read the FAI files from the file of files"
